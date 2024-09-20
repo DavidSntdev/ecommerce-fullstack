188d871a2382357a10b6629dde344c842645ca3b
David Santos <162488821+DavidSntdev@users.noreply.github.com>
2024-09-20 02:04:13 -0300

    Update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { dev }) => {
+    if (config.cache && !dev) {
+      config.cache = Object.freeze({
+        type: "memory",
+      });
+    }
+    // Important: return the modified config
+    return config;
+  },
+};
 
 export default nextConfig;
